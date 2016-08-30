223a5558ef5fe8a76623ebd0ee89839a28594af5
Radiergummi <m@9dev.de>
2016-08-30 18:33:20 +0200

    added stylesheet handling with gulp

--- src/meta/javascripts.js
+++ src/meta/javascripts.js
@@ -5,28 +5,44 @@
  require
  */
 
-var webpack                    = require('webpack'),
-    nconf                      = require('nconf'),
-    path                       = require('path'),
-    winston                    = require('winston'),
-    Javascripts                = module.exports = {},
+var webpack     = require('webpack'),
+    nconf       = require('nconf'),
+    path        = require('path'),
+    winston     = require('winston'),
+    Javascripts = module.exports = {},
     debug = (nconf.get('environment') === 'development');
 
 Javascripts.deploymentPath = path.join(nconf.get('path'), nconf.get('assets:js'));
 Javascripts.deploymentUri  = '/javascripts/';
 
+// load all required webpack plugins
 var compilerPlugins = [
+
+  // move common code into base scripts
   new webpack.optimize.CommonsChunkPlugin((debug ? 'base.js' : 'base.min.js'))
 ];
 
-if (!debug) {
+// enable minification in production
+if (! debug) {
   compilerPlugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: { warnings: false }
   }));
 }
 
+/**
+ * create the webpack instance
+ */
 var compiler = webpack({
+  debug:   debug,
+
+  /**
+   * the public javascript path
+   */
   context: path.join(Javascripts.deploymentPath, 'src'),
+
+  /**
+   * the app entry points. That'll be scripts for individual pages
+   */
   entry:   {
     'dashboard':        './dashboard.js',
     'invoices.all':     './invoices.all.js',
@@ -49,20 +65,32 @@ var compiler = webpack({
         : '[name].min.map'
                        )
   },
+
+  // enable caching
   cache:   true,
-  debug:   debug,
+
+  // render source maps only in development
   devtool: (debug ? 'source-map' : false),
   plugins: compilerPlugins,
-  module: {
+  module:  {
     loaders: [
+
+      /**
+       * ignore translation JSON files
+       */
       { test: /\/translations\/.{2,5}\.json$/, loader: 'ignore-loader' }
     ]
   }
 });
 
-Javascripts.compile = function() {
-  return new Promise(function(resolve, reject) {
-    compiler.run(function(error, stats) {
+/**
+ * compiles the public javascript files
+ *
+ * @returns {Promise}
+ */
+Javascripts.compile = function () {
+  return new Promise(function (resolve, reject) {
+    compiler.run(function (error, stats) {
       if (error) {
         return reject(error);
       }
@@ -79,7 +107,7 @@ Javascripts.compile = function() {
         winston.info(stats.toString({ colors: true }));
       }
 
-      return resolve(null);
+      return resolve();
     });
   })
 };
--- src/meta/stylesheets.js
+++ src/meta/stylesheets.js
@@ -5,142 +5,27 @@
  require
  */
 
-var path = require('path'),
-    fs = require('fs'),
+var gulp     = require('gulp'),
+    scss     = require('gulp-scss'),
+    nconf    = require('nconf'),
+    path     = require('path'),
+    basePath = nconf.get('path');
 
-    nconf = require('nconf'),
-    cleanCss = require('clean-css'),
-    mkdirp = require('mkdirp'),
-    moment = require('moment'),
-    prependFile = require('prepend-file'),
-    rimraf = require('rimraf'),
-    sass = require('node-sass'),
-    winston = require('winston'),
-
-    SCSS = module.exports = {},
-
-    basePath = nconf.get('path'),
-    sassPath = path.join(basePath, nconf.get('assets').sass),
-    cssPath = path.join(basePath, nconf.get('assets').css);
-
-
-SCSS.compile = function () {
-  winston.info('[libs/stylesheets]'.white + ' Compiling SASS');
-
-  // read the scss file folder
-  var files = fs.readdirSync(sassPath);
-
-  // recreate css folder
-  rimraf.sync(cssPath);
-  mkdirp.sync(cssPath);
-
-  // filter scss files
-  files = files.filter(function (filename) {
-    var extension = filename.slice(-5);
-    return (extension === '.scss' || extension === '.sass');
-  });
-
-  for (var file in files) {
-    if (! files.hasOwnProperty(file)) continue;
-
-    var filename = path.join(sassPath, files[ file ]),
-        basePath = path.join(cssPath, files[ file ].substring(0, files[ file ].length - 5)),
-        outputPath = basePath + '.css',
-        sourceMapPath = basePath + '.css.map',
-        result = sass.renderSync({
-          file:              filename,
-          indentedSyntax:    false,
-          indentType:        'space',
-          indentWidth:       2,
-          linefeed:          'lf',
-          omitSourceMapUrl:  false,
-          outFile:           outputPath,
-          outputStyle:       'nested',
-          precision:         3,
-          sourceComments:    (nconf.get('environment') === 'development'),
-          sourceMap:         true,
-          sourceMapContents: true,
-          sourceMapEmbed:    false,
-          sourceMapRoot:     '/css'
-        });
-
-    // write compiled css file
-    fs.writeFileSync(outputPath, result.css);
-
-    // write source map file
-    fs.writeFileSync(sourceMapPath, result.map);
-
-
-    // add debug data and statistics in dev mode
-    if (nconf.get('environment') === 'development') {
-      var debugHeader = "/*\n Compiled at " + moment().format() + "\n";
-      debugHeader += JSON.stringify(result.stats, null, 2) + "\n*/\n\n";
-      prependFile.sync(outputPath, debugHeader);
-    }
-  }
-
-  winston.info('[libs/stylesheets]'.white + ' Successfully compiled SCSS files');
-  return this;
+var Stylesheets = module.exports = {
+  sourcePath:     path.join(basePath, 'public', 'scss'),
+  deploymentPath: path.join(basePath, 'public', 'css')
 };
 
-SCSS.minify = function () {
-  winston.info('[libs/stylesheets]'.white + ' Minifying CSS');
-  var files = fs.readdirSync(cssPath);
+Stylesheets.compile = function () {
+  return new Promise(function (resolve, reject) {
+    var gulpStream = gulp.src([
+      path.join(Stylesheets.sourcePath, '*.scss')
+    ])
+      .pipe(scss().on('error', function (error) {
+        reject(error);
+      }))
+      .pipe(gulp.dest(Stylesheets.deploymentPath));
 
-  // filter scss files
-  files = files.filter(function (filename) {
-    // skip minified files
-    if (filename.indexOf('.min.') !== - 1) return false;
-
-    // only return css files
-    return (filename.slice(- 4) === '.css');
+    return resolve(gulpStream);
   });
-
-  for (var file in files) {
-    if (files.hasOwnProperty(file) === false) continue;
-
-    var filename = path.join(cssPath, files[ file ]),
-        basePath = path.join(cssPath, files[ file ].substring(0, files[ file ].length - 4)),
-        outputPath = basePath + '.min.css',
-        sourceMapPath = basePath + '.css.map',
-        result = new cleanCss({
-          advance:                true,
-          aggressiveMerging:      false,
-          benchmark:              false,
-          compatibility:          'ie8',
-          debug:                  (nconf.get('environment') === 'development'),
-          keepBreaks:             false,
-          keepSpecialComments:    '*',
-          mediaMerging:           true,
-          processImport:          true,
-          processImportFrom:      [ 'local' ],
-          rebase:                 true,
-          restructuring:          true,
-          // relativeTo: '/', // path to rebase relative links to
-          // root: '/', // path to rebase absolute links to
-          target:                 cssPath, // path to a folder or an output file to which rebase
-                                           // all URLs
-          roundingPrecision:      3,
-          semanticMerging:        false,
-          shorthandCompacting:    true,
-          sourceMap:              fs.readFileSync(basePath + '.css.map').toString(),
-          sourceMapInlineSources: true
-        }).minify(fs.readFileSync(filename));
-
-    // write minifed css file
-    fs.writeFileSync(outputPath, result.styles);
-
-    // write source map file
-    fs.writeFileSync(sourceMapPath, result.sourceMap);
-
-    // add debug data and statistics in dev mode
-    if (nconf.get('environment') === 'development') {
-      var debugHeader = "/*\n Minified at " + moment().format() + "\n";
-      debugHeader += JSON.stringify(result.stats, null, 2) + "\n*/\n\n";
-      prependFile.sync(outputPath, debugHeader);
-    }
-  }
-
-  winston.info('[libs/stylesheets]'.white + ' Successfully minified CSS files');
-  return this;
 };
