f4472aa5b20902eec253f04f7d701fef654bdac2
Radiergummi <m@9dev.de>
2016-09-07 17:17:45 +0200

    added getConfig API controller

--- src/controllers/api/user.js
+++ src/controllers/api/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var fs        = require('fs'),
+var file        = require('../../meta/file'),
     nconf     = require('nconf'),
 
     userModel = require('../../models/user'),
@@ -9,11 +9,44 @@ var fs        = require('fs'),
 var userApi = module.exports = {};
 
 userApi.savePicture = function(req, res, next) {
-  fs.writeFile(nconf.get('path') + '/public/images/users/' + req.body.user + '.jpg', req.file.buffer, function(error) {
+  file.write('/public/images/users/' + req.body.user + '.jpg', req.file.buffer, function(error) {
     if (error) {
       return next(error);
     }
 
     return res.status(204).send({});
   });
+};
+
+userApi.getConfig = function(req, res, next) {
+  var config = {
+    debug: (nconf.get('environment') === 'development')
+  };
+
+  if (!req.user) {
+    config.language = nconf.get('language');
+    config.user     = {};
+
+    return res.json(config);
+  }
+
+  userModel.findOne({ _id: req.user._id }, function(error, data) {
+    if (error) {
+      return res.status(500).json({
+        status: 500,
+        reason: 'db-error',
+        message: {
+          raw: 'could not fetch user configuration data: ' + error.message,
+          translation: '[[global:server_error]]'
+        }
+      });
+    }
+
+    config.language               = data.language || nconf.get('language');
+    config.user                   = JSON.parse(JSON.stringify(data));
+    config.user.hasProfilePicture = file.existsSync('public/images/users/' + socket._id + '.jpg');
+
+    return res.json(config);
+  });
+
 };
