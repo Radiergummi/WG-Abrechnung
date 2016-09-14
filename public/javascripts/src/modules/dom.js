7f855e1d05bbcfb38aee03367d8a970536f83fdb
Radiergummi <m@9dev.de>
2016-09-14 11:52:01 +0200

    fixed DOM framework, implemented selector API

--- public/javascripts/src/modules/dom.js
+++ public/javascripts/src/modules/dom.js
@@ -1,37 +1,44 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   /**
+   * turns an arbitrary set of DOM nodes into an array
    *
-   * @param {Array|Element|HTMLElement|Node|HTMLCollection|NodeList} elements
+   * @param {Array|HTMLCollection|NodeList|Element|HTMLElement|Node} elements
    *
    * @returns {Array}
    * @private
    */
-  function _covertToElementArray (elements) {
+  function _covertToElementArray(elements) {
 
     // if we have not received a list of selectors, convert it
-    if (!elements.hasOwnProperty('length')) {
-      elements = [ elements ];
+    if (
+      elements instanceof Element ||
+      elements instanceof HTMLElement ||
+      elements instanceof Node
+    ) {
+      return [ elements ];
+    }
+
+    if (elements.hasOwnProperty('length') && elements.length === 1) {
+      return [ elements[ 0 ] ];
     }
 
     // if selectors is not an array, convert it
     if (typeof elements !== 'Array') {
-      elements = Array.prototype.slice.call(elements);
+      return Array.prototype.slice.call(elements);
     }
-
-    return elements;
   }
 
   /**
+   * parses a selector string
    *
    * @param {string|Element|HTMLElement|Node} selector
-   * @returns {Element|HTMLElement|Node|Array}
+   * @returns {Array}
    * @private
    */
-  function _parseSelector (selector) {
-    var elements;
+  function _parseSelector(selector) {
 
     // if we received any type of DOM node, return it
     if (typeof selector === 'object' && selector.hasOwnProperty('nodeType')) {
@@ -71,67 +78,165 @@ module.exports = function(app) {
     return _covertToElementArray(document.querySelectorAll(selector));
   }
 
-  function _DOMElement (elementSet) {
-    console.log(elementSet);
+  /**
+   * constructor for the DOM Element abstractor
+   *
+   * @param {Array} elementSet
+   * @returns {_DOMElement}
+   * @private
+   */
+  function _DOMElement(elementSet) {
     this.elementSet = elementSet;
 
-    for (var i = 0; i < elementSet.length; i++) {
+    for (var i = 0; i < elementSet.length; i ++) {
       this[ i ] = elementSet[ i ];
     }
 
-    this.length = elementSet.length;
+    this.length = {
+      get: function() {
+        var count = 0,
+            idx   = 0;
+
+        while (this[ idx ]) {
+          count++;
+          idx++;
+        }
+
+        return count;
+      }
+    };
 
     // return all elements
     return this;
   }
 
+  /**
+   * defines the DOMElement class prototype
+   * 
+   * @type {{length: number, splice: _DOMElement.splice}}
+   */
   _DOMElement.prototype = {
     length: 0,
-    splice: function(begin, end) {
+    splice: function (begin, end) {
       return this.elementSet.slice(begin, end);
     }
   };
-/*
-  Object.defineProperty(_DOMElement.prototype, 'length', {
-    get: function() {
-      var count = 0,
-          idx   = 0;
 
-      while (this[ idx ]) {
-        count++;
-        idx++;
-      }
-
-      return count;
+  /**
+   * iterates over each element in the current set
+   * 
+   * @param   {function} callback
+   * @returns {_DOMElement}
+   */
+  _DOMElement.prototype.each = function (callback) {
+    for (var index = 0; index < this.length; index ++) {
+      callback.call(this, index, this.elementSet[ index ]);
     }
-  });
-*/
-  _DOMElement.prototype.each = function(callback) {
-    for (var index = 0; index < this.elementSet; index++) {
-      callback.call(this, this.elementSet[ index ], index);
+
+    return this;
+  };
+
+  /**
+   * returns the last element in the set as a new dom element
+   *
+   * @returns {_DOMElement}
+   */
+  _DOMElement.prototype.first = function () {
+    return new _DOMElement([ this.elementSet[ 0 ] ]);
+  };
+
+  /**
+   * returns the first element in the set as a new dom element
+   * 
+   * @returns {_DOMElement}
+   */
+  _DOMElement.prototype.last = function () {
+    return new _DOMElement([ this.elementSet[ this.length - 1 ] ]);
+  };
+
+  /**
+   * attaches an event listener using the apps event framework
+   *
+   * @param {string}   eventNames
+   * @param {function} [callback]
+   * @param {object}   [options]
+   */
+  _DOMElement.prototype.on = function (eventNames, callback, options) {
+    return this.each(function() {
+      return app.on(eventNames, this, callback, options);
+    });
+  };
+
+  /**
+   * removes an event listener using the apps event framework
+   *
+   * @param {string}   eventNames
+   * @param {function} [callback]
+   */
+  _DOMElement.prototype.off = function (eventNames, callback) {
+    return this.each(function() {
+      return app.off(eventNames, this, callback);
+    });
+  };
+
+  /**
+   * adds a class
+   *
+   * @param   {string} className
+   * @returns {_DOMElement}
+   */
+  _DOMElement.prototype.addClass = function(className) {
+    return this.each(function() {
+      return this.classList.add(className);
+    });
+  };
+
+  /**
+   * removes a class
+   *
+   * @param   {string} className
+   * @returns {_DOMElement}
+   */
+  _DOMElement.prototype.removeClass = function(className) {
+    return this.each(function() {
+      return this.classList.remove(className);
+    });
+  };
+
+  /**
+   * toggles a class
+   *
+   * @param   {string} className
+   * @returns {_DOMElement}
+   */
+  _DOMElement.prototype.toggleClass = function(className) {
+    return this.each(function() {
+      return this.classList.toggle(className);
+    });
+  };
+
+  /**
+   * wrapper function for the DOM library (minimal jQuery-like interface)
+   *
+   * @param   {*} selector
+   * @returns {*}
+   */
+  app.dom = function (selector) {
+    if (!selector) {
+      return this;
     }
+
+    var elements = _parseSelector(selector);
+    return new _DOMElement(elements);
   };
 
-  _DOMElement.prototype.on = function(eventNames, callback, options) {
-    return app.on(eventNames, this, callback, options);
-  };
-
-  _DOMElement.prototype.off = function(eventNames, callback) {
-    return app.off(eventNames, this, callback);
-  };
-
-  app.dom = function(selector) {
-    return new _DOMElement(_parseSelector(selector));
-  };
-
-
-  app.helpers.createNode = function(tagName, attributes, content) {
+  app.helpers.createNode = function (tagName, attributes, content) {
     var node = document.createElement(tagName);
 
     attributes = attributes || {};
 
     for (var attr in attributes) {
-      if (!attributes.hasOwnProperty(attr)) {
+      if (! attributes.hasOwnProperty(attr)) {
         continue;
       }
 
@@ -149,23 +254,23 @@ module.exports = function(app) {
     return node;
   };
 
-  app.helpers.createElement = function(elementString) {
+  app.helpers.createElement = function (elementString) {
     var parser = new DOMParser();
 
     return parser.parseFromString(elementString, 'text/html').body.childNodes[ 0 ];
   };
 
-  app.helpers.createTranslatedElement = function(elementString) {
+  app.helpers.createTranslatedElement = function (elementString) {
     var parser = new DOMParser();
 
-    return new Promise(function(resolve) {
-      app.translator.translate(elementString, app.config.language, function(translatedElementString) {
+    return new Promise(function (resolve) {
+      app.translator.translate(elementString, app.config.language, function (translatedElementString) {
         resolve(parser.parseFromString(translatedElementString, 'text/html').body.childNodes[ 0 ]);
       });
     });
   };
 
-  app.events.imageError = function(image) {
+  app.events.imageError = function (image) {
     image.onerror = '';
     image.src     = '/images/noImage.svg';
     image.classList.add('image-error');
--- public/javascripts/src/settings.js
+++ public/javascripts/src/settings.js
@@ -4,12 +4,13 @@ var app  = require('./app'),
     main = require('./main')(app);
 
 
-(function(app) {
-  app.startup.push(function() {
+(function (app) {
+  app.startup.push(function () {
     require('./libraries/vanilla-modal')(app);
 
     app.elements.userList               = document.getElementsByClassName('users')[ 0 ];
-    app.elements.sendInvitationButton   = document.getElementsByClassName('send-invite') [ 0 ];
+    //app.elements.sendInvitationButton   = document.getElementsByClassName('send-invite') [ 0 ];
+    app.elements.sendInvitationButton   = app.dom('.send-invite');
     app.elements.invitationEmailAddress = document.getElementById('invite-email-address');
     app.elements.deleteUserButton       = document.getElementsByClassName('delete-user')[ 0 ];
     app.elements.saveUserButton         = document.getElementsByClassName('save-user')[ 0 ];
@@ -17,18 +18,18 @@ var app  = require('./app'),
     /**
      * attaches the invitation event listeners
      */
-    app.listeners.addInvitationEvents = function() {
-      app.on('click', app.elements.sendInvitationButton, app.events.sendInvitation);
+    app.listeners.addInvitationEvents = function () {
+      app.elements.sendInvitationButton.on('click', app.events.sendInvitation);
     };
 
     /**
      * attaches the delete user event listeners
      */
-    app.listeners.addDeleteUserEvents = function() {
+    app.listeners.addDeleteUserEvents = function () {
       app.on('click', app.elements.deleteUserButton, app.events.deleteUser);
     };
 
-    app.listeners.addEditUserEvents = function() {
+    app.listeners.addEditUserEvents = function () {
 //      app.on('click')
     };
 
@@ -37,7 +38,7 @@ var app  = require('./app'),
      *
      * @param {Event} event
      */
-    app.events.sendInvitation = function(event) {
+    app.events.sendInvitation = function (event) {
 
       // retrieve the email address from its field
       var emailAddress = app.elements.invitationEmailAddress.value;
@@ -50,11 +51,11 @@ var app  = require('./app'),
         event.target.classList.add('sending');
 
         // dispatch the invitation via the appropriate socket connector
-        app.connectors.sendInvitation(emailAddress, function(error, response) {
+        app.connectors.sendInvitation(emailAddress, function (error, response) {
           if (error) {
 
             // if we have an error, show a notification
-            app.translate('[[settings:user_management.invite.notification_error, ' + error.message + ']]', function(translated) {
+            app.translate('[[settings:user_management.invite.notification_error, ' + error.message + ']]', function (translated) {
               app.notifications.error(translated);
             });
 
@@ -70,12 +71,12 @@ var app  = require('./app'),
           }
 
           // everything went smoothly, show a notification
-          app.translate('[[settings:user_management.invite.notification_success, ' + response.invitation.sentTo + ']]', function(translated) {
+          app.translate('[[settings:user_management.invite.notification_success, ' + response.invitation.sentTo + ']]', function (translated) {
             app.notifications.success(translated);
           });
 
           // leave the user some time to see what's going on
-          return setTimeout(function() {
+          return setTimeout(function () {
 
             // remove the sending class and enable the button again
             event.target.classList.remove('sending');
@@ -88,7 +89,7 @@ var app  = require('./app'),
       } else {
 
         // the validation failed, show a notification
-        app.translate('[[settings:user_management.invite.notification_valid_email]]', function(translated) {
+        app.translate('[[settings:user_management.invite.notification_valid_email]]', function (translated) {
           app.notifications.warning(translated);
         });
       }
@@ -99,7 +100,7 @@ var app  = require('./app'),
      *
      * @param {Event} event
      */
-    app.events.prepareDeleteUserModal = function(event) {
+    app.events.prepareDeleteUserModal = function (event) {
       app.debug('attaching prepare delete user modal data');
       var modal = document.getElementsByClassName('dialog-delete-user-wrapper')[ 0 ];
 
@@ -115,23 +116,23 @@ var app  = require('./app'),
      *
      * @param {Event} event
      */
-    app.events.deleteUser = function(event) {
+    app.events.deleteUser = function (event) {
 
       // prevent users from deleting themselves
       if (event.target.dataset.userId === app.config.user._id) {
 
         // if we have an error, show a notification
-        app.translate('[[settings:user_management.delete.notification_error_delete_self]]', function(translated) {
+        app.translate('[[settings:user_management.delete.notification_error_delete_self]]', function (translated) {
           app.notifications.error(translated);
           app.modals.instance.close();
         });
       }
 
-      app.connectors.deleteUser(event.target.dataset.userId, function(error, deletedUser) {
+      app.connectors.deleteUser(event.target.dataset.userId, function (error, deletedUser) {
         if (error) {
 
           // if we have an error, show a notification
-          app.translate('[[settings:user_management.delete.notification_error, ' + error.message + ']]', function(translated) {
+          app.translate('[[settings:user_management.delete.notification_error, ' + error.message + ']]', function (translated) {
             app.notifications.error(translated);
           });
 
@@ -143,11 +144,11 @@ var app  = require('./app'),
         }
 
         // everything went smoothly, show a notification
-        app.translate('[[settings:user_management.delete.notification_success, ' + deletedUser.firstName + ' ' + deletedUser.lastName + ']]', function(translated) {
+        app.translate('[[settings:user_management.delete.notification_success, ' + deletedUser.firstName + ' ' + deletedUser.lastName + ']]', function (translated) {
           app.notifications.success(translated);
         });
 
-        return setTimeout(function() {
+        return setTimeout(function () {
           app.modals.instance.close();
 
           // delete the user row
@@ -156,8 +157,8 @@ var app  = require('./app'),
       });
     };
 
-    app.events.prepareEditUserModal = function(event) {
-      return app.connectors.getUserDetails(event.target.dataset.userId, function(user) {
+    app.events.prepareEditUserModal = function (event) {
+      return app.connectors.getUserDetails(event.target.dataset.userId, function (user) {
         var modal  = document.getElementsByClassName('modal-edit-user-wrapper')[ 0 ],
             fields = {
               firstName: document.getElementById('modified-first-name'),
@@ -187,10 +188,10 @@ var app  = require('./app'),
      * @param {string} emailAddress
      * @param {function(Error, object)} callback
      */
-    app.connectors.sendInvitation = function(emailAddress, callback) {
+    app.connectors.sendInvitation = function (emailAddress, callback) {
 
       // emit the socket call
-      app.io.emit('settings.sendInvitation', emailAddress, function(error, response) {
+      app.io.emit('settings.sendInvitation', emailAddress, function (error, response) {
         if (error) {
           return callback(error, response);
         }
@@ -206,12 +207,12 @@ var app  = require('./app'),
      * @param {function(Error, object)} callback
      * @returns {*}
      */
-    app.connectors.deleteUser = function(userId, callback) {
-      if (!userId) {
+    app.connectors.deleteUser = function (userId, callback) {
+      if (! userId) {
         return callback(new Error('invalid user ID ' + userId), null);
       }
 
-      app.io.emit('settings.deleteUser', userId, function(error, deletedUser) {
+      app.io.emit('settings.deleteUser', userId, function (error, deletedUser) {
         if (error) {
           return callback(error, null);
         }
@@ -220,10 +221,10 @@ var app  = require('./app'),
       });
     };
 
-    app.connectors.createUser = function(userData, callback) {
+    app.connectors.createUser = function (userData, callback) {
 
 
-      app.post('/api/users', userData, function(response) {
+      app.post('/api/users', userData, function (response) {
         if (response.ok) {
           return callback(response.responseText);
         } else {
@@ -232,8 +233,8 @@ var app  = require('./app'),
       });
     };
 
-    app.connectors.getUserDetails = function(userId, callback) {
-      return app.http.get('/api/users/' + userId, function(response) {
+    app.connectors.getUserDetails = function (userId, callback) {
+      return app.http.get('/api/users/' + userId, function (response) {
         if (response.ok) {
           return callback(response.responseText);
         } else {
@@ -242,8 +243,8 @@ var app  = require('./app'),
       });
     };
 
-    app.connectors.editUser = function(userId, userData, callback) {
-      return app.http.put('/api/users/' + userId, userData, function(response) {
+    app.connectors.editUser = function (userId, userData, callback) {
+      return app.http.put('/api/users/' + userId, userData, function (response) {
         if (response.ok) {
           return callback(response.responseText);
         } else {
